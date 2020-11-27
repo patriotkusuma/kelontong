95365aa2dedf5a80e6f9e37884e64ada41540ace
patriotkusuma <patriot.kusumasejati@gmail.co>
2020-11-28 00:13:20 +0700

    Products Table Migrations

---
+++ app/Database/Migrations/2020-11-27-164106_products.php
@@ -0,0 +1,61 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Products extends Migration
+{
+	public function up()
+	{
+		// Attribute Table Produk
+		$this->forge->addField([
+			'product_id'			=> [
+				'type'				=> 'BIGINT',
+				'constraint'		=> 20,
+				'unsignedd'			=> TRUE,
+				'auto_increment'	=> TRUE,
+			],
+			'sku'					=> [
+				'type'				=> 'BIGINT',
+				'constraint'		=> 13,
+			],
+			'name'					=> [
+				'type'				=> 'VARCHAR',
+				'constraint'		=> 250,
+			],
+			'product_slug'			=> [
+				'type'				=> 'VARCHAR',
+				'constraint'		=> 250,
+			],
+			'category_id'			=> [
+				'type'				=> 'BIGINT',
+				'constraint'		=> 20,
+			],
+			'harga'					=> [
+				'type'				=> 'REAL',
+				'constraint'		=> 20,
+				'null'				=> TRUE,
+			],
+			'stok'					=> [
+				'type'				=> 'int',
+				'constraint'		=> 11,
+				'null'				=> TRUE,
+			],
+			'status'				=> [
+				'type'				=> 'ENUM',
+				'constraint'		=> "'ACTIVE', 'INACTIVE",
+				'default'			=> 'ACTIVE'
+			],
+		]);
+
+		$this->forge->addPrimaryKey('product_id');
+		$this->forge->addUniqueKey('sku');
+		$this->forge->addUniqueKey('product_slug')
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		//
+	}
+}
