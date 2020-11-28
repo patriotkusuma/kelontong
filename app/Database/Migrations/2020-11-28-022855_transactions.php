161ea240254add4c98c3a82a22f86dd4967871a9
patriotkusuma <patriot.kusumasejati@gmail.co>
2020-11-28 09:41:42 +0700

    Transactions Table Migrations

---
+++ app/Database/Migrations/2020-11-28-022855_transactions.php
@@ -0,0 +1,54 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Transactions extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'transaction_id'			=> [
+				'type'					=> 'BIGINT',
+				'constraint'			=> 20,
+				'unsigned'				=> TRUE,
+				'auto_increment'		=> TRUE,
+			],
+			'user_id'					=> [
+				'type'					=> 'BIGINT',
+				'constraint'			=> 20,
+				'null'					=> TRUE,
+			],
+			'detail_transaction_id'		=> [
+				'type'					=> 'BIGINT',
+				'constraint'			=> 20,
+				'null'					=> TRUE,
+			],
+			'date'						=> [
+				'type'					=> 'DATETIME',
+				'null'					=> TRUE,
+			],
+			'sub_total'					=> [
+				'type'					=> 'INT',
+				'constraint'			=> 11,
+				'null'					=> TRUE,
+			],
+			'grand_total'				=> [
+				'type'					=> 'BIGINT',
+				'constraint'			=> 20,
+				'null'					=> TRUE,
+			]
+		]);
+
+		$this->forge->addPrimaryKey('transaction_id');
+		$this->forge->addForeignKey('user_id','users','user_id','CASCADE','CASCADE');
+		$this->forge->addForeignKey('detail_transactions_id','detail_transactions','detail_transactions_id','CASCADE','CASCADE');
+		$this->forge->createTable('transactions');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		//
+	}
+}
