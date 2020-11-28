cef80120700b209f550c46e30037a8261da0964f
patriotkusuma <patriot.kusumasejati@gmail.co>
2020-11-28 09:57:23 +0700

    Categories Controller

---
+++ app/Controllers/Categories.php
@@ -0,0 +1,16 @@
+<?php namespace App\Controllers;
+use CodeIgniter\Controller;
+use App\Models\Categories_model;
+
+class Categories extends BaseController
+{
+	public function index()
+	{
+        $model = new Categories_model();
+        $data['categories'] = $model->getCategories();
+        return view('category/index', $data);
+	}
+
+	//--------------------------------------------------------------------
+
+}
