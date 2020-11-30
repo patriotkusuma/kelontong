33e070f976f719a4c43ef696a71cebd626f0402f
patriotkusuma <patriot.kusumasejati@gmail.co>
2020-12-01 04:23:52 +0700

    Changing

--- app/Views/admin/category/index.php
+++ app/Views/admin/category/index.php
@@ -1,6 +1,5 @@
 <?= $this->section('custom_css') ?>
     <!-- DataTables -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
 
 <?= $this->endSection() ?>
