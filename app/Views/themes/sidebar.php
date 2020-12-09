1b599c274050a0fccf86fc72329c25eee701a915
patriotkusuma <patriot.kusumasejati@gmail.co>
2020-12-10 02:31:42 +0700

    Adding Logo Image

--- app/Views/themes/sidebar.php
+++ app/Views/themes/sidebar.php
@@ -4,7 +4,7 @@ $uri = $request->uri->getSegment(2) ?>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?= base_url() ?>" class="brand-link">
-        <img src="<?= base_url() ?>/themes/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?= base_url() ?>/img/LogoKelontong.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Kelontong</span>
     </a>
 
