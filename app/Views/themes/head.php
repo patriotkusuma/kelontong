e508f6168586f4bfc9a1e97952e42a6172a7e4eb
patriotkusuma <patriot.kusumasejati@gmail.co>
2020-12-09 23:57:54 +0700

    Adding Icon

--- app/Views/themes/head.php
+++ app/Views/themes/head.php
@@ -5,7 +5,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="<?= base_url('favicon.ico') ?>">
 
     <title><?= isset($title) ? $title : 'Kelontong' ?></title>
 
